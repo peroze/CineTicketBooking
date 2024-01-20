f2d68b60008d3fcb716662342a1ed7eccc25adc6
Athanasios Efstathiou <145462138+a-efstathiou@users.noreply.github.com>
2024-01-20 17:58:32 +0200

    Spring initializr
    Initialize the spring mvc

---
+++ src/main/java/com/unipi/CineTicketBooking/CineTicketBookingApplication.java
@@ -0,0 +1,13 @@
+package com.unipi.CineTicketBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineTicketBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineTicketBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unipi/CineTicketBooking/CineTicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unipi.CineTicketBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineTicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
