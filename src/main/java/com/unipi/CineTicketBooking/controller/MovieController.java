42dd092655e144685520c577b501b170dbfe372a
Athanasios Efstathiou <a.efstathiou@proton.me>
2024-01-23 03:37:27 +0200

    CRUD operations on movieController. added server include message to always appear for the exceptions that get thrown in my methods if something is not right

--- src/main/java/com/unipi/CineTicketBooking/controller/MovieController.java
+++ src/main/java/com/unipi/CineTicketBooking/controller/MovieController.java
@@ -1,9 +1,10 @@
 package com.unipi.CineTicketBooking.controller;
 
+import com.unipi.CineTicketBooking.model.Movie;
 import com.unipi.CineTicketBooking.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/movie")
@@ -16,8 +17,25 @@ public class MovieController {
         this.movieService = movieService;
     }
 
+    @GetMapping
+    public List<Movie> getMovies(){
+        return movieService.getMovies();
+    }
 
+    @PostMapping
+    public void addNewMovie(@RequestBody Movie movie){
+        movieService.addNewMovie(movie);
+    }
 
+    @DeleteMapping(path = "{movieName}")
+    public void deleteMovie(@PathVariable("movieName") String name){
+        movieService.deleteMovie(name);
+    }
+
+    @PutMapping(path = "{movieName}")
+    public void updateMovie(@PathVariable("movieName") String name, @RequestBody Movie newMovie) {
+        movieService.updateMovie(name, newMovie);
+    }
 
 
 }
--- src/main/java/com/unipi/CineTicketBooking/service/MovieService.java
+++ src/main/java/com/unipi/CineTicketBooking/service/MovieService.java
@@ -2,10 +2,12 @@ package com.unipi.CineTicketBooking.service;
 
 import com.unipi.CineTicketBooking.model.Movie;
 import com.unipi.CineTicketBooking.repository.MovieRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -22,4 +24,54 @@ public class MovieService {
     }
 
 
+    public void addNewMovie(Movie movie) {
+        Optional<Movie> movieOptional = movieRepository.findMovieByName(movie.getName());
+        if(movieOptional.isPresent()){
+            throw new IllegalStateException("name already in list");
+        }
+        movieRepository.save(movie);
+    }
+
+
+    public void deleteMovie(String name) {
+        boolean exists = movieRepository.existsByName(name);
+        if(!exists){
+            throw new IllegalStateException("Movie with name "+name+" does not exists");
+        }
+        movieRepository.deleteByName(name);
+    }
+
+    @Transactional
+    public void updateMovie(String movieName, Movie newMovie) {
+        Movie movie = movieRepository.findMovieByName(movieName).orElseThrow(() -> new IllegalStateException(
+                "movie with name "+movieName+ " does not exist"
+        ));
+
+        if(newMovie.getGenre() != null){
+            movie.setGenre(newMovie.getGenre());
+        }
+        if (newMovie.getDuration() != null) {
+            movie.setDuration(newMovie.getDuration());
+        }
+        if (newMovie.getReleaseDate() != null) {
+            movie.setReleaseDate(newMovie.getReleaseDate());
+        }
+        if (newMovie.getDescription() != null) {
+            movie.setDescription(newMovie.getDescription());
+        }
+        if (newMovie.getDirector() != null) {
+            movie.setDirector(newMovie.getDirector());
+        }
+        if (newMovie.getActors() != null) {
+            movie.setActors(newMovie.getActors());
+        }
+        if (newMovie.getRating() != null) {
+            movie.setRating(newMovie.getRating());
+        }
+        if (newMovie.getLanguage() != null) {
+            movie.setLanguage(newMovie.getLanguage());
+        }
+
+
+    }
 }
