dbf24ca4ed69d427060217bfe0ba112b310b9b6f
Lamprini <rikoula4@gmail.com>
2024-01-24 19:17:06 +0200

    added new files

---
+++ src/main/java/com/unipi/CineTicketBooking/controller/BookingsController.java
@@ -0,0 +1,49 @@
+package com.unipi.CineTicketBooking.controller;
+
+import org.springframework.web.bind.annotation.*;
+import com.unipi.CineTicketBooking.model.Bookings;
+import com.unipi.CineTicketBooking.service.BookingsService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/bookings")
+
+public class BookingsController {
+    private final BookingsService bookingsService;
+
+    @Autowired
+    public BookingsController(BookingsService bookingsService) {
+        this.bookingsService = bookingsService;
+    }
+
+    @GetMapping("/all")
+    public List<Bookings> getAllBookings() {
+        return bookingsService.getAllBookings();
+    }
+    @GetMapping("/{id}")
+    public Bookings getBookingById(@PathVariable Long id) {
+        return bookingsService.getBookingById(id);
+    }
+
+    @PostMapping("/create")
+    public Bookings createBooking(@RequestBody Bookings booking) {
+        return bookingsService.createBooking(booking);
+    }
+
+    @PutMapping("/update/{id}")
+    public Bookings updateBooking(@PathVariable Long id, @RequestBody Bookings updatedBooking) {
+        return bookingsService.updateBooking(id, updatedBooking);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteBooking(@PathVariable Long id) {
+        bookingsService.deleteBooking(id);
+    }
+
+}
--- src/main/java/com/unipi/CineTicketBooking/service/BookingsService.java
+++ src/main/java/com/unipi/CineTicketBooking/service/BookingsService.java
@@ -3,12 +3,14 @@ package com.unipi.CineTicketBooking.service;
 import com.unipi.CineTicketBooking.model.Bookings;
 import com.unipi.CineTicketBooking.repository.BookingsRepository;
 import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+import java.util.List;
 
 @Service
 public class BookingsService {
 
     private final BookingsRepository bookingsRepository;
-
 
     public BookingsService(BookingsRepository bookingsRepository) {
         this.bookingsRepository = bookingsRepository;
@@ -18,4 +20,36 @@ public class BookingsService {
 
         return bookingsRepository.save(booking);
     }
+
+    public List<Bookings> getAllBookings() {
+        List<Bookings> bookingsList = bookingsRepository.findAll();
+        return bookingsList;
+    }
+    public Bookings getBookingById(Long id) {
+        Optional<Bookings> optionalBooking = bookingsRepository.findById(id);
+        return optionalBooking.orElse(null);
+    }
+    public Bookings updateBooking(Long id, Bookings updatedBooking) {
+        Optional<Bookings> optionalBooking = bookingsRepository.findById(id);
+
+        if (optionalBooking.isPresent()) {
+            Bookings booking = optionalBooking.get();
+            return bookingsRepository.save(booking);
+        } else {
+            return null;
+        }
+    }
+    public boolean deleteBooking(Long id) {
+        Optional<Bookings> optionalBooking = bookingsRepository.findById(id);
+
+        if (optionalBooking.isPresent()) {
+            bookingsRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
 }
