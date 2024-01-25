6be3585e6134b8b841d14cded5cbaa9d171ec194
Athanasios Efstathiou <a.efstathiou@proton.me>
2024-01-25 19:44:52 +0200

    added the google authentication

--- FrontEnd/cine-ticket-booking-app/src/App.js
+++ FrontEnd/cine-ticket-booking-app/src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { GoogleOAuthProvider} from '@react-oauth/google';
+import {GoogleLogin} from '@react-oauth/google';
+import {jwtDecode} from 'jwt-decode';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+           Hi,Welcome to the frontend
+
+      <GoogleOAuthProvider 
+      clientId="813061530558-eumv2pprpivufvqth4352l5qoo9mtj7b.apps.googleusercontent.com">
+        <GoogleLogin
+          onSuccess={credentialResponse => {
+            const decoded = jwtDecode(credentialResponse.credential);
+            console.log(decoded);
+            //console.log(credentialResponse);
+          }}
+          onError={() => {
+            console.log('Login Failed');
+          }}
+          cancel_on_tap_outside={true}
+          size="large"
+          shape="pill"
+        />
+      </GoogleOAuthProvider>
     </div>
   );
 }
