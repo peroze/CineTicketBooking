48016f4678991fbe5327e5889b4dbc675a4f3e82
Peroze <konper98@gmail.com>
2024-01-26 04:32:56 +0200

    NavMenuAndBootstrap

--- FrontEnd/cine-ticket-booking-app/src/App.js
+++ FrontEnd/cine-ticket-booking-app/src/App.js
@@ -3,30 +3,17 @@ import './App.css';
 import { GoogleOAuthProvider} from '@react-oauth/google';
 import {GoogleLogin} from '@react-oauth/google';
 import {jwtDecode} from 'jwt-decode';
+import Menubar from './Components/menubar.jsx';
 
 
 
 function App() {
   return (
-    <div className="App">
-           Hi,Welcome to the frontend
+    <div className="App p-1" >
 
-      <GoogleOAuthProvider 
-      clientId="72971430335-bpegei5og5285e6dhuiqcegp8661ffof.apps.googleusercontent.com">
-        <GoogleLogin
-          onSuccess={credentialResponse => {
-            const decoded = jwtDecode(credentialResponse.credential);
-            console.log(decoded);
-            //console.log(credentialResponse);
-          }}
-          onError={() => {
-            console.log('Login Failed');
-          }}
-          cancel_on_tap_outside={true}
-          size="large"
-          shape="pill"
-        />
-      </GoogleOAuthProvider>
+      <Menubar></Menubar>
+      
+      
     </div>
   );
 }
---
+++ FrontEnd/cine-ticket-booking-app/src/Components/menubar.jsx
@@ -0,0 +1,35 @@
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../App.css";
+import logo from './Images/Logo.png';
+import "../App.css";
+import { Component } from 'react';
+import styles from"./Style/navbar.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faUser} from'@fortawesome/free-regular-svg-icons'
+import {faFilm} from'@fortawesome/free-solid-svg-icons'
+import Container from 'react-bootstrap/Container';
+
+
+
+function Menubar() {
+    return (
+        <Navbar expand="lg" bg="#77ACA2" variant="light" className="rounded-pill m-3 p-0 navbar" >
+          <Container>
+            <Navbar.Brand href="#home"><img width="150"  className="Navbarlogo" src={logo}></img></Navbar.Brand>
+            <Navbar.Toggle  aria-controls="basic-navbar-nav"><FontAwesomeIcon className="burger" icon={faFilm}/></Navbar.Toggle>
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto gap-5 ">
+              <Nav.Link   href="#home">Home</Nav.Link>
+              <Nav.Link   href="#link">Movies</Nav.Link>
+              <Nav.Link   href="#link">Contact</Nav.Link>
+              <Nav.Link   href="#link"><FontAwesomeIcon className="fic" icon={faUser}/></Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+    </Navbar>
+    );
+  }
+  
+  export default Menubar;
