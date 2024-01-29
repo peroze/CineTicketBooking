e908c49e58ea7447bd40e9ce72ec868474020a0c
Lamprini <rikoula4@gmail.com>
2024-01-29 17:49:26 +0200

    added rooms

--- src/main/java/com/unipi/CineTicketBooking/controller/BookingsController.java
+++ src/main/java/com/unipi/CineTicketBooking/controller/BookingsController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
@@ -23,28 +25,40 @@ public class BookingsController {
     }
 
     @GetMapping("/all")
-    public List<Bookings> getAllBookings() {
-        return bookingsService.getAllBookings();
+    public ResponseEntity<List<Bookings>> getAllBookings() {
+        List<Bookings> bookings = bookingsService.getAllBookings();
+        return new ResponseEntity<>(bookings, HttpStatus.OK);
     }
     @GetMapping("/{id}")
-    public Bookings getBookingById(@PathVariable Long id) {
-        return bookingsService.getBookingById(id);
+    public ResponseEntity<Bookings> getBookingById(@PathVariable Long id) {
+        Bookings booking = bookingsService.getBookingById(id);
+        if (booking != null) {
+            return new ResponseEntity<>(booking, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @PostMapping("/create")
-    public Bookings createBooking(@RequestBody Bookings booking) {
-
-        return bookingsService.createBooking(booking);
+    public ResponseEntity<Bookings> createBooking(@RequestBody Bookings booking) {
+        Bookings createdBooking = bookingsService.createBooking(booking);
+        return new ResponseEntity<>(createdBooking, HttpStatus.CREATED);
     }
 
     @PutMapping("/update/{id}")
-    public Bookings updateBooking(@PathVariable Long id, @RequestBody Bookings updatedBooking) {
-        return bookingsService.updateBooking(id, updatedBooking);
+    public ResponseEntity<Bookings> updateBooking(@PathVariable Long id, @RequestBody Bookings updatedBooking) {
+        Bookings booking = bookingsService.updateBooking(id, updatedBooking);
+        if (booking != null) {
+            return new ResponseEntity<>(booking, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @DeleteMapping("/delete/{id}")
-    public void deleteBooking(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteBooking(@PathVariable Long id) {
         bookingsService.deleteBooking(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
