5394ba8af96ca1a3969ef35bf1c8cdbdaf412d2e
Athanasios Efstathiou <a.efstathiou@proton.me>
2024-02-06 20:11:17 +0200

    created AuthenticationService, AuthenticationController and CookieAuthenticationFilter classes

--- src/main/java/com/unipi/CineTicketBooking/config/SecurityConfiguration.java
+++ src/main/java/com/unipi/CineTicketBooking/config/SecurityConfiguration.java
@@ -1,15 +1,22 @@
 package com.unipi.CineTicketBooking.config;
 
-import jakarta.servlet.Filter;
+import jakarta.annotation.Resource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.RememberMeAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.RememberMeServices;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.rememberme.RememberMeAuthenticationFilter;
+import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Configuration
 @EnableWebSecurity
@@ -18,13 +25,30 @@ public class SecurityConfiguration {
 
     private final JwtAuthenticationFilter JwtAuthenticationFilter;
     private final AuthenticationProvider authenticationProvider;
+
+
+    @Resource
+    UserDetailsService userDetailsService;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
 
         httpSecurity.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth
                 .requestMatchers("api/users/login","api/users/register").permitAll()
                 .anyRequest().authenticated())
-                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).authenticationProvider(authenticationProvider).addFilterBefore(JwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).authenticationProvider(authenticationProvider)
+                    .addFilterBefore(JwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                    .addFilterBefore(new CookieAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .rememberMe(rememberMe ->
+                        rememberMe
+                                .key("your-secret-key")
+                                .userDetailsService(userDetailsService)
+                                .tokenValiditySeconds(60 * 60 * 24 * 365)
+                                .rememberMeParameter("rememberMe")
+                                .useSecureCookie(true)
+                )
+                .logout(logout -> logout.deleteCookies(CookieAuthenticationFilter.COOKIE_NAME))
+        ;
         return httpSecurity.build();
     }
 }
