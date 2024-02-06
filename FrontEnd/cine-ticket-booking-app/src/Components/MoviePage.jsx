8d89a5227e3d0a33e6fda6900b31f07be6e891df
Athanasios Efstathiou <a.efstathiou@proton.me>
2024-02-06 22:02:22 +0200

    axios requests

--- FrontEnd/cine-ticket-booking-app/src/Components/MoviePage.jsx
+++ FrontEnd/cine-ticket-booking-app/src/Components/MoviePage.jsx
@@ -5,16 +5,33 @@ import { useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faAngleLeft} from '@fortawesome/free-solid-svg-icons'
 import { useNavigate } from 'react-router-dom';
-
+import axios from 'axios';
 
 
 
 const MoviePage = () => {
     const location=useLocation();
     const id=location.state;
-    console.log(id);
     const naviagte=useNavigate();
     var movie
+
+    axios({
+        method: 'get',
+        url: 'http://localhost:8080/api/movies/',
+        params: {
+            id: String(id)
+        }
+    })
+    .then(function (response) {
+        console.log(response.data);
+        
+        
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+
     if(id==1){
          movie=new Movie("The Avengers: Age of Ultron",1,"Superhero Movie",120,"04/02/2024","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.","Josh Wedon",["Robert Downey Junior","Scarlett Johansson","Chris Evans"],"PG-13");
     }
--- FrontEnd/cine-ticket-booking-app/src/components/Movies.jsx
+++ FrontEnd/cine-ticket-booking-app/src/components/Movies.jsx
@@ -7,12 +7,28 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route,Header } from "react-router-dom";
 import MoviePage from './MoviePage.jsx';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 
 
 const Movies = () => {
   const naviagte=useNavigate();
-  const movies=[{id:2,name:"The Avengers"},{id:3,name:"Mission: Impossible Fallout"},{id:1,name:"The Avengers: Age of Ultron"}]
+  //const movies=[{id:2,name:"The Avengers"},{id:3,name:"Mission: Impossible Fallout"},{id:1,name:"The Avengers: Age of Ultron"}]
+  const movies =[];
+  axios.get('http://localhost:8080/api/movies/movieList')
+  .then(function (response) {
+    console.log(response.data);
+    const movieData = response.data;
+    movieData.forEach(movie => {
+      movies.push(movie);
+    });
+    console.log(movies);
+      
+  })
+  .catch(function (error) {
+      console.log(error);
+  });
+
   return (
     <div>
           <h1>Movies</h1>
@@ -24,7 +40,7 @@ const Movies = () => {
               <MovieCard  name={movie.name} id={movie.id}></MovieCard>
             </div>
             ))}
-            <div className="cards" onClick={()=>{
+             <div className="cards" onClick={()=>{
                 naviagte('/movie',{state:1})
             }}><MovieCard name={'The Avengers 2'} id={1}/></div>
             <div className="cards"onClick={()=>{
@@ -32,7 +48,7 @@ const Movies = () => {
             }}><MovieCard name={'The Avengers'} id={2}/></div>
             <div className="cards"onClick={()=>{
                 naviagte('/movie',{state:3})
-            }}><MovieCard name={'Mission: Impossible Fallout'} id={3}/> </div>
+            }}><MovieCard name={'Mission: Impossible Fallout'} id={3}/> </div> 
         </div>
 
       
