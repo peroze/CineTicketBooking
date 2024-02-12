369c48f1091d932dc3a8e4b428cdb032d8c47289
Athanasios Efstathiou <a.efstathiou@proton.me>
2024-02-12 19:03:56 +0200

    access token and refresh token

--- src/main/java/com/unipi/CineTicketBooking/service/UsersService.java
+++ src/main/java/com/unipi/CineTicketBooking/service/UsersService.java
@@ -1,10 +1,8 @@
 package com.unipi.CineTicketBooking.service;
 
 import com.unipi.CineTicketBooking.config.JwtService;
-import com.unipi.CineTicketBooking.controller.secondaryClasses.LoginRequest;
 import com.unipi.CineTicketBooking.model.Users;
 import com.unipi.CineTicketBooking.repository.UsersRepository;
-import jdk.swing.interop.SwingInterOpUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -35,13 +33,6 @@ public class UsersService {
         usersRepository.save(user);
         var jwtToken=jwtService.generateToken(user);
         return jwtToken;
-    }
-
-    public String login(LoginRequest request){
-        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(),request.getPassword()));
-        Users user=usersRepository.findUsersByEmail(request.getUsername()).orElseThrow(); //If it reaches this point then the user is correct
-        var jwtToken=jwtService.generateToken(user);
-        return  jwtToken;
     }
 
     public List<Users> getAllUsers() { return usersRepository.findAll();
