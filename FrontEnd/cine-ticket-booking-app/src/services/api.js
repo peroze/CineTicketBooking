7659ab8003f73239b8e3d7cd0bdd63eaa63db7ce
Athanasios Efstathiou <a.efstathiou@proton.me>
2024-02-12 19:05:27 +0200

    axios instance and services

---
+++ FrontEnd/cine-ticket-booking-app/src/services/api.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import TokenService from "./token.service.js";
+
+const instance = axios.create({
+  baseURL: "http://localhost:8080/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    const token = TokenService.getLocalAccessToken();
+    if (token) {
+      config.headers["Authorization"] = 'Bearer ' + token;  // for Spring Boot back-end
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  async (err) => {
+    const originalConfig = err.config;
+
+    if (originalConfig.url !== "/auth/authenticate" && err.response) {
+      // Access Token was expired
+      if (err.response.status === 401 && !originalConfig._retry) {
+        originalConfig._retry = true;
+
+        try {
+          const rs = await instance.post("/auth/refresh-token", {
+            refresh_token: TokenService.getLocalRefreshToken(),
+          });
+
+          const { access_token } = rs.data;
+          TokenService.updateLocalAccessToken(access_token);
+
+          return instance(originalConfig);
+        } catch (_error) {
+          return Promise.reject(_error);
+        }
+      }
+    }
+
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
+
