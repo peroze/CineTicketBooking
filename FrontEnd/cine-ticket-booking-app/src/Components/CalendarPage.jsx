7c5feee18e27fa96a2af5de6570a3bf2b81a28cb
Lamprini <rikoula4@gmail.com>
2024-02-19 19:44:56 +0200

    added the calendar page

--- FrontEnd/cine-ticket-booking-app/src/Components/CalendarPage.jsx
+++ FrontEnd/cine-ticket-booking-app/src/Components/CalendarPage.jsx
@@ -24,8 +24,8 @@ function formatDate(date) {
 console.log(formatDate(new Date().toDateString()));
 const currentDate = formatDate(new Date().toDateString());
 const schedulerData = [
-  { startDate: '2024-02-14T17:30', endDate: '2024-02-14T19:30', title: 'Play1' },
-  { startDate: '2024-02-14T20:00', endDate: '2024-02-14T22:30', title: 'Play2' },
+  { startDate: '2024-02-20T17:30', endDate: '2024-02-20T19:30', title: 'Play1' },
+  { startDate: '2024-02-20T20:00', endDate: '2024-02-20T22:30', title: 'Play2' },
 ];
 const CalendarPage = () => {
   const [date, setDate] = useState(new Date());
