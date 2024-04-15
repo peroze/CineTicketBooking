892a394bc7340ef447ff13ac072aeb4f177e66fa
Athanasios Efstathiou <a.efstathiou@proton.me>
2024-04-15 20:53:15 +0300

    glogin

--- FrontEnd/cine-ticket-booking-app/src/components/GoogleLogin.jsx
+++ FrontEnd/cine-ticket-booking-app/src/components/GoogleLogin.jsx
@@ -4,18 +4,23 @@ import { GoogleOAuthProvider, GoogleLogin, useGoogleLogin } from '@react-oauth/g
 import {jwtDecode} from 'jwt-decode';
 import axios from 'axios';
 import AuthService from "../services/auth.service";
+import { FcGoogle } from "react-icons/fc";
 
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 
-const GoogleLoginButton = ({ onLoginComplete }) => {
+import './Style/GoogleLogin.css'; // Import the external CSS file
+
+const GoogleLoginButton = ({ onLoginComplete, className }) => {
 
   const googleLogin = useGoogleLogin({
     flow: 'auth-code',
     scope: "openid profile email",
     onSuccess: async (codeResponse) => {
       try{
+        console.log(codeResponse);
         const response = await axios.get(`http://localhost:8080/api/oauth2/code/google?code=${codeResponse.code}`);
+        console.log(response);
         AuthService.oauth2login(response);
         //call the function that handles the login and pass the user email that authenticated
         onLoginComplete(response.data.user_email,response.data.image_url,response.data.isSignUp);
@@ -31,16 +36,10 @@ const GoogleLoginButton = ({ onLoginComplete }) => {
   return (
     <Container className='custom-google-btn d-flex flex-column justify-content-md-center align-items-center'>
         
-        <GoogleLogin
-            onSuccess={handleLoginSuccess}
-            onError={handleLoginError}
-            cancel_on_tap_outside={true}
-            size="large"
-            shape="rectangular"
-            theme="dark"
-        />
-
-        <Button onClick={googleLogin}>googleLogin</Button>
+        <Button onClick={googleLogin} className={`custom-Google-btn ${className}`}>
+        <FcGoogle className="custom-google-icon"/>
+          Continue with Google
+        </Button>
 
         
         
